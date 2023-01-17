d8546f2676fe565da9bb2e449d86a8cface94703
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-01-17 10:36:59 +0200

    credentials does not work with providers :)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,24 +1,28 @@
 import NextAuth, { NextAuthOptions, User } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import { signIn } from 'next-auth/react';
+// Prisma
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '../../../lib/prismadb';
+import Email from 'next-auth/providers/email';
 
 export const authOptions: NextAuthOptions = {
-    pages: {
-        error: '/auth/error',
-    },
-    callbacks: {
-        signIn({ user, account }) {
-            const isAllowedToSignIn = true;
-            if (account?.provider == 'google') {
-                // Check if user in database
-                // for now lets check if its my email
-                if (user.email == 'mariam.shabrawy.uni1@gmail.com') return true;
-                else return false;
-            }
-            return isAllowedToSignIn;
-        },
-    },
+    adapter: PrismaAdapter(prisma),
+    // pages: {
+    //     error: '/auth/error',
+    // },
+    // callbacks: {
+    //     signIn({ user, account }) {
+    //         const isAllowedToSignIn = true;
+    //         if (account?.provider == 'google') {
+    //             // Check if user in database
+    //             // for now lets check if its my email
+    //             if (user.email == 'mariam.shabrawy.uni1@gmail.com') return true;
+    //             else return false;
+    //         }
+    //         return isAllowedToSignIn;
+    //     },
+    // },
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
@@ -36,14 +40,18 @@ export const authOptions: NextAuthOptions = {
             },
             authorize: async (credentails, req) => {
                 // Check if user exists in database
-                if (
-                    credentails?.email == 'admin@gmail.com' &&
-                    credentails.password == '0000'
-                ) {
-                    let u: User = { id: '237', email: credentails.email };
-                    return u;
-                }
-                return null;
+                const usr = await prisma.user.findFirst({
+                    where: {
+                        email: credentails?.email,
+                    },
+                });
+                if (!usr) return null;
+                // if (usr.password !== credentails?.password) return null;
+
+                return {
+                    id: String(usr.id),
+                    email: usr.email,
+                };
             },
         }),
     ],
