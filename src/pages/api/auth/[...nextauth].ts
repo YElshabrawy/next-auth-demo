207e5c243d2e902af2fd534702782291e2c98259
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-01-17 08:20:44 +0200

    credentials provider working

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import NextAuth, { NextAuthOptions, User } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { signIn } from 'next-auth/react';
 
 export const authOptions: NextAuthOptions = {
@@ -22,6 +23,28 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'email',
+                    placeholder: 'jsmith@test.com',
+                },
+                password: { label: 'Password', type: 'password' },
+            },
+            authorize: async (credentails, req) => {
+                // Check if user exists in database
+                if (
+                    credentails?.email == 'admin@gmail.com' &&
+                    credentails.password == '0000'
+                ) {
+                    let u: User = { id: '237', email: credentails.email };
+                    return u;
+                }
+                return null;
+            },
         }),
     ],
 };
